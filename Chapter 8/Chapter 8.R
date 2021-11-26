c1fb8782853b0465d3ca6ab93110a64084faac7c
Nicolas Kepper <nicolas.kepper@gmail.com>
2021-11-26 22:19:49 +0100

    Finish Ch 8 Code

--- Chapter 8/Chapter 8.R
+++ Chapter 8/Chapter 8.R
@@ -114,3 +114,77 @@ lines(rugged_seq, mu.mean, lwd=2)
 shade(mu.ci, rugged_seq)
 mtext("Non-African nations")
 
+
+rugged_seq <- seq(from=-.2, to=1.2, length.out=30)
+muA <- link(m8.3, data=data.frame(cid=1, rugged_std=rugged_seq))
+muN <- link(m8.3, data=data.frame(cid=2, rugged_std=rugged_seq))
+delta <- muA - muN
+
+delta.mean <- apply(delta, 2, mean)
+delta.ci <- apply(delta, 2, PI, prob=.97)
+
+
+plot(NULL, xlab="ruggedness", ylab="Expected difference African and non African nation", xlim=range(rugged_seq), ylim=c(-0.3, 0.2))
+lines(rugged_seq, delta.mean, lwd=2)
+shade(delta.ci, rugged_seq)
+
+
+rm(list=ls())
+
+### 8.3
+
+data(tulips)
+d <- tulips
+str(d)
+
+d$blooms_std <- d$blooms / max(d$blooms)
+d$water_cent <- d$water - mean(d$water)
+d$shade_cent <- d$shade - mean(d$shade)
+
+m8.4 <- quap(
+  alist(
+    blooms_std ~ dnorm(mu, sigma),
+    mu <- a + bw * water_cent + bs * shade_cent,
+    a ~ dnorm(.5, .25),
+    bw ~ dnorm(0, .25),
+    bs ~ dnorm(0, .25),
+    sigma ~ dexp(1)
+  ), data=d
+)
+
+m8.5 <- quap(
+  alist(
+    blooms_std ~ dnorm(mu, sigma),
+    mu <- a + bw * water_cent + bs * shade_cent + bws * water_cent * shade_cent,
+    a ~ dnorm(.5, .25),
+    bw ~ dnorm(0, .25),
+    bs ~ dnorm(0, .25),
+    bws ~ dnorm(0, .25),
+    sigma ~ dexp(1)
+  ), data=d
+)
+
+# Triptych plot
+
+par(mfrow=c(2, 3))
+for(model in c(m8.4, m8.5)) {
+  for(s in -1:1) {
+    idx <- which(d$shade_cent == s)
+    plot(d$water_cent[idx], d$blooms_std[idx], xlim=c(-1, 1), ylim=c(0, 1), 
+         xlab="water", ylab="blooms", pch=16, col=rangi2)
+    mu <- link(model, data=data.frame(shade_cent=s, water_cent=-1:1))
+    for(i in 1:20) lines(-1:1,  mu[i, ], col=col.alpha("black", .3))
+  }
+}
+
+par(mfrow=c(2, 3))
+for(model in c(m8.4, m8.5)) {
+  prior <- extract.prior(model)
+  for(s in -1:1) {
+    idx <- which(d$shade_cent == s)
+    plot(d$water_cent[idx], d$blooms_std[idx], xlim=c(-1, 1), ylim=c(0, 1), 
+         xlab="water", ylab="blooms", pch=16, col=rangi2)
+    mu <- link(model, post=prior, data=data.frame(shade_cent=s, water_cent=-1:1))
+    for(i in 1:20) lines(-1:1,  mu[i, ], col=col.alpha("black", .3), lwd=ifelse(i==1, 3, 1))
+  }
+}
